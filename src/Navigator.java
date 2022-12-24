5f4544ce11228add3d3cc4f971b06a8ba61e1ba9
SASAN <info.hdgame@gmail.com>
2022-12-24 18:53:10 +0330

    initial commit

---
+++ src/Navigator.java
@@ -0,0 +1,12 @@
+/**
+ * This class Navigate us to target classes
+ */
+public class Navigator {
+    /**
+     * Lets Goooo
+     * @param args
+     */
+    public static void main (String args [] ){
+        System.out.println("Navigator");
+    }
+}
